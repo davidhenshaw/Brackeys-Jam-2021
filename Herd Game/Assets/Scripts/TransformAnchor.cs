f0d474894a3855d5341e4582b2a32ecf92bcb0fb
David Henshaw <davidhenshaw2014@gmail.com>
2021-02-15 17:05:21 -0600

    Create Transformation Anchor

---
+++ Herd Game/Assets/Scripts/TransformAnchor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace metakazz.Util{
+    public class TransformAnchor : ScriptableObject
+    {
+        [HideInInspector]
+        public bool isSet = false; 
+
+        private Transform _transform;
+        public Transform Transform
+        {
+            get { return _transform; }
+            set
+            {
+                _transform = value;
+                isSet = _transform != null;
+            }
+        }
+
+        public void OnDisable()
+        {
+            _transform = null;
+            isSet = false;
+        }
+    }
+}
